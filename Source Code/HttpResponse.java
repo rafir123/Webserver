6c2b9fa337ac1f0f028c20bf6ca304b3d6a1f092
Rafi Rafsan <30943851+rafir123@users.noreply.github.com>
2020-01-14 23:55:29 -0500

    Add files via upload

---
+++ Source Code/HttpResponse.java
@@ -0,0 +1,50 @@
+import java.util.Map;
+
+public class HttpResponse {
+
+    private int code;
+    private String status;
+    private Map<String, Object> headers;
+    private String message;
+    private String version;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Map<String, Object> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(Map<String, Object> headers) {
+        this.headers = headers;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
